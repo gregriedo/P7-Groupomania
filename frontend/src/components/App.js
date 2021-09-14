ecd9b897b59c5d821500d5945da3b3bf15090c1b
gregriedo <gregory.riedo@gmail.com>
2021-09-14 09:41:39 +0200

    ajout fichier App.js dans dossier frontend

---
+++ frontend/src/components/App.js
@@ -0,0 +1,32 @@
+import "/Users/lhommesanscoeur/github/P7-Groupomania/frontend/src/App.css";
+import Banner from "/Users/lhommesanscoeur/github/P7-Groupomania/frontend/src/components/banner.js";
+import "fontsource-roboto";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import HomePage from "../pages/home";
+import ConnectForm from "../pages/login";
+import CreateAccount from"../pages/signup";
+import UserBoard from"../pages/userBoard";
+
+import MultilineTextFields from "../pages/newArticle";
+import MultilineTextFieldsComment from "../pages/newComment";
+
+function App() {
+  return (
+    <HashRouter>
+      <Banner />
+      <Switch>
+        
+        <Route path="/newArticle" component={MultilineTextFields} />
+        <Route path="/newComment" component={MultilineTextFieldsComment} />
+        <Route path="/userBoard" component={UserBoard} />
+        <Route path="/login" component={ConnectForm} />
+        <Route path="/signup" component={CreateAccount} />
+        <Route path="/" component={HomePage} />
+
+
+      </Switch>
+    </HashRouter>
+  );
+}
+
+export default App;
