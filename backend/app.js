2a913abf95359d46646e26733ea3f2d2d05f3056
gregriedo <gregory.riedo@gmail.com>
2021-09-14 09:33:58 +0200

    ajout fichier app.js dans dossier backend

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const articleRoutes = require('./routes/articles');
+const commentRoutes = require('./routes/comments');
+const userRoutes = require('./routes/users');
+
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/article', articleRoutes);
+app.use('/api/comment', commentRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
