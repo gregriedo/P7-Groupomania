8483d99d7e277320b0bd026484f3035f3be0ddb5
gregriedo <gregory.riedo@gmail.com>
2021-09-14 09:38:32 +0200

    ajout fichier comments.js dans sous dossier routes

---
+++ backend/routes/comments.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+
+
+const commentCtrl = require('../controllers/comments.js');
+
+
+router.post('/', commentCtrl.createNewComment);
+router.get('/',  commentCtrl.getAllComment);
+router.delete('/:id', auth, commentCtrl.deleteComment);
+
+
+module.exports = router;
