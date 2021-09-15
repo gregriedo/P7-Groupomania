5a50a12cba82aff0dd9a758d95b886168ef07a3f
gregriedo <gregory.riedo@gmail.com>
2021-09-15 15:09:41 +0200

    modification code dans userboard.js

--- frontend/src/pages/userBoard.js
+++ frontend/src/pages/userBoard.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState} from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
+
 import Typography from "@material-ui/core/Typography";
-import MenuIcon from "@material-ui/icons/Menu";
+
 import Link from "@material-ui/core/Link";
 
 import Button from '@material-ui/core/Button';
@@ -27,9 +27,7 @@ const useStyles = makeStyles((theme) => ({
   grow: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
+
   title: {
     display: "none",
     [theme.breakpoints.up("sm")]: {
@@ -43,12 +41,7 @@ const useStyles = makeStyles((theme) => ({
       display: "flex",
     },
   },
-  sectionMobile: {
-    display: "flex",
-    [theme.breakpoints.up("md")]: {
-      display: "none",
-    },
-  },
+ 
 }));
 
 export default function PrimarySearchAppBar({ history }) {
@@ -95,35 +88,31 @@ export default function PrimarySearchAppBar({ history }) {
        <div className={classes.grow}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
             <div>{userR.username}</div>
           </Typography>
+          
+          
+          
+        
         </Toolbar>
       </AppBar>
-    
-      <nav>
-        <div className={classes.roots}>
-          <Link color="primary" href="#/newArticle" className={classes.link}>
-            Nouvel Article
-          </Link>
-          <Button className="button" onClick={deleteAccount} type="submit" variant="contained" color="secondary">Supprimer Compte</Button>
-        </div>
-      </nav>
-    
+      
    
       <div className="article">
         <RecipeReviewCard />
       </div>
       
-    
+      <footer>
+      <div className="deleteButton">
+          <Button className="button" onClick={deleteAccount} type="submit" variant="contained" color="secondary">Supprimer Compte</Button>
+      </div>
+      <div className="link">
+          <Link color="primary" href="#/newArticle" className={classes.link}>
+            Nouvel Article
+          </Link>
+      </div>    
+      </footer>
     </div>
     
   );
